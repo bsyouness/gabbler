0d9b5fe2ea0002fb83495f82f2f201f46f62aa91
Heiko Seeberger <heiko.seeberger@gmail.com>
2014-06-05 22:42:21 +0200

    Set up project

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,12 @@
 import sbt._
 
 object Version {
-  val akka      = "2.3.3"
+  val akka      = "2.3.4"
   val logback   = "1.1.2"
   val scala     = "2.11.1"
-  val scalaTest = "2.1.7"
+  val scalaTest = "2.2.0"
+  val spray     = "1.3.1"
+  val sprayJson = "1.2.6"
 }
 
 object Library {
@@ -13,6 +15,9 @@ object Library {
   val akkaTestkit    = "com.typesafe.akka" %% "akka-testkit"    % Version.akka
   val logbackClassic = "ch.qos.logback"    %  "logback-classic" % Version.logback
   val scalaTest      = "org.scalatest"     %% "scalatest"       % Version.scalaTest
+  val sprayCan       = "io.spray"          %% "spray-can"       % Version.spray
+  val sprayJson      = "io.spray"          %% "spray-json"      % Version.sprayJson
+  val sprayRouting   = "io.spray"          %% "spray-routing"   % Version.spray
 }
 
 object Dependencies {
@@ -20,6 +25,13 @@ object Dependencies {
   import Library._
 
   val gabbler = List(
+    akkaActor,
+    akkaSlf4j,
+    logbackClassic,
+    sprayCan,
+    sprayJson,
+    sprayRouting,
+    akkaTestkit % "test",
     scalaTest % "test"
   )
 }
